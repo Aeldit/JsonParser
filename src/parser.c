a306bf3963ff3515f44f4883af65209826ab8092
aeldit <raphael.roger.92@gmail.com>
2024-05-22 18:25:41 +0200

    Add is_num and is_bool functions

--- src/parser.c
+++ src/parser.c
@@ -1,25 +1,109 @@
 #include "parser.h"
 
+/*******************************************************************************
+**                                  INCLUDES                                  **
+*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "lists/linked_lists.h"
 
+/*******************************************************************************
+**                              LOCAL FUNCTIONS                               **
+*******************************************************************************/
+char is_str_number(char *str, size_t len)
+{
+    if (str == NULL || len == 0)
+    {
+        return 0;
+    }
+
+    for (size_t i = 0; i < len; ++i)
+    {
+        // If not a minus sign ('-') or a number, we return false
+        if (str[i] != '-' && (str[i] < '0' || '9' < str[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+/**
+** \return 1 if the given string is "true", 2 if the given string is "false",
+**         and 0 otherwise
+*/
+char is_str_boolean(char *str, size_t len)
+{
+    if (str == NULL || len < 4 || len > 5)
+    {
+        return 0;
+    }
+
+    if (len == 4 && str[0] == 't' && str[1] == 'r' && str[2] == 'u'
+        && str[3] == 'e')
+    {
+        return 1;
+    }
+    if (len == 5 && str[0] == 'f' && str[1] == 'a' && str[2] == 'l'
+        && str[3] == 's' && str[4] == 'e')
+    {
+        return 2;
+    }
+    return 0;
+}
+
+long str_to_long(char *str, size_t len)
+{
+    if (str == NULL || len == 0)
+    {
+        return 0;
+    }
+    long res = 0;
+    char is_negative = 1;
+
+    for (size_t i = 0; i < len; ++i)
+    {
+        if (str[i] == '-')
+        {
+            is_negative = -1;
+        }
+        else if ('0' <= str[i] && str[i] <= '9')
+        {
+            res = res * 10 + str[i] - '0';
+        }
+    }
+    return res * is_negative;
+}
+
 void handle_non_str_value(json_dict_st *jd, char *key, char *value)
 {
+    size_t len = strlen(value);
+
     // Booleans
-    if (!strcmp(value, "true"))
+    char is_boolean = is_str_boolean(value, len);
+    if (is_boolean == 1)
     {
         add_bool(jd, key, 1);
     }
-    else if (!strcmp(value, "false"))
+    else if (is_boolean == 2)
     {
         add_bool(jd, key, 0);
+    }
+    else
+    {
+        if (is_str_number(value, len))
+        {
+            add_num(jd, key, str_to_long(value, len));
+        }
     }
     free(value);
 }
 
+/*******************************************************************************
+**                                 FUNCTIONS                                  **
+*******************************************************************************/
 json_dict_st *parse(char *file)
 {
     FILE *f = fopen(file, "r");
@@ -48,7 +132,6 @@ json_dict_st *parse(char *file)
         return NULL;
     }
 
-    // Possible elements
     char *key = NULL;
 
     char c = '\0';
@@ -140,6 +223,7 @@ json_dict_st *parse(char *file)
             {
                 if (!s.is_in_str)
                 {
+                    // Last pair of the json object
                     if (s.is_in_value)
                     {
                         s.is_in_value = 0;
